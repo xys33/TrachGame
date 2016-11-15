53cdb35136bebed6a5638c8e9c2414dc8b147a01
xys33 <krzysztofkowol01@gmail.com>
2016-11-16 00:07:29 +0100

    chat autoclearing problem, working methods

--- client/templates/chat/chat.js
+++ client/templates/chat/chat.js
@@ -9,21 +9,23 @@ if (Meteor.isClient) {
       'keydown input#message' : function (event) {
         if (event.which == 13) { // 13 is the enter key event
           if (Meteor.user())
-            var name = Meteor.user().profile.name;
+            var name = Meteor.user().username;
           else
             var name = 'Anonymous';
+
           var message = document.getElementById('message');
+          console.log(Meteor.user().username, message);
           if (message.value != '') {
-            Messages.insert({
-              name: name,
-              message: message.value,
-              time: Date.now(),
+            Meteor.call('insertMessage',name,message.value,function(err, result) {
+                if (err) return Errors.throw(err.reason);
             });
 
-            document.getElementById('message').value = '';
+            //document.getElementById('message').value = '';
             message.value = '';
           }
         }
       }
     }
 }
+
+
